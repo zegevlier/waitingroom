9d46f11ffee2e03de6e7650796a006035c093820
zegevlier <iamascratcher@gmail.com>
2024-07-09 11:51:53 +0200

    Update stresser

--- waitingroom-stresser/src/main.rs
+++ waitingroom-stresser/src/main.rs
@@ -1,8 +1,5 @@
-use std::sync::Arc;
-
 use futures::stream::StreamExt;
 use reqwest::header::HeaderValue;
-use reqwest_cookie_store::{CookieStore, CookieStoreMutex};
 
 async fn sleep_until_next_refresh(header: &HeaderValue) {
     let refresh = header.to_str().unwrap();
@@ -15,16 +12,29 @@ async fn run_clients(count: usize, parallelism_per_thread: usize) {
     futures::stream::iter(0..count)
         .map(move |i| async move {
             println!("Starting client {}", i);
-            let cookie_store = CookieStore::new(None);
-            let cookie_store = Arc::new(CookieStoreMutex::new(cookie_store));
 
             let client = reqwest::Client::builder()
-                .cookie_provider(Arc::clone(&cookie_store))
+                .pool_max_idle_per_host(1)
+                .cookie_store(true)
                 .build()
                 .unwrap();
 
             loop {
-                let response = client.get("http://127.0.0.1:8000/").send().await.unwrap();
+                let mut errors = 0;
+                let response = loop {
+                    let response = match client.get("http://127.0.0.1:8000/").send().await {
+                        Ok(r) => r,
+                        Err(e) => {
+                            errors += 1;
+                            if errors > 5 {
+                                panic!("Too many errors: {}", e);
+                            }
+                            println!("Error: {}", e);
+                            continue;
+                        }
+                    };
+                    break response;
+                };
                 let wr_status_header = response.headers().get("x-wr-status").unwrap();
                 let wr_status = wr_status_header.to_str().unwrap();
 
@@ -45,22 +55,36 @@ async fn run_clients(count: usize, parallelism_per_thread: usize) {
             }
         })
         .buffer_unordered(parallelism_per_thread)
-        .collect::<Vec<_>>()
+        .all(|_| async { true })
         .await;
 }
 
 #[tokio::main(flavor = "multi_thread", worker_threads = 10)]
 async fn main() {
-    let count = 1000;
-    let parallelism_per_thread = 50;
+    let per_thread_count = 200;
+    let parallelism_per_thread = 100;
     let thread_count = 10;
 
-    let mut thread_handles = Vec::new();
+    let start_time = std::time::Instant::now();
+    let mut handles = Vec::new();
     for _ in 0..thread_count {
-        thread_handles.push(tokio::spawn(async move {
-            run_clients(count / thread_count, parallelism_per_thread).await;
-        }));
+        handles.push(tokio::spawn(run_clients(
+            per_thread_count,
+            parallelism_per_thread,
+        )));
+        tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
     }
 
-    futures::future::join_all(thread_handles).await;
+    futures::future::join_all(handles).await;
+    let elapsed = start_time.elapsed();
+
+    println!(
+        "Processed a total of {} users",
+        per_thread_count * thread_count
+    );
+    println!("Elapsed time: {:?}", elapsed);
+    println!(
+        "Users per second: {}",
+        per_thread_count * thread_count / elapsed.as_secs() as usize
+    );
 }
